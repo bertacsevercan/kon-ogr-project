8b1c8e2f59561d3a6385d958cedf5d43cccc1aa1
bertacsevercan <bertac.severcan@gmail.com>
2021-01-06 23:26:47 +0300

    import containers

--- portfolyo/src/App.js
+++ portfolyo/src/App.js
@@ -1,13 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import Home from "./containers/Home";
+import Contact from "./containers/Contact";
+import Projects from "./containers/Projects";
+import About from "./containers/About";
+import Navbar from "./components/Navbar";
+import Comments from "./containers/Comments";
 import './App.css';
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
     <div className="App">
-   
+      <Navbar />
+      <Home />
+      <Projects />
+      <About />
+      <Comments />
+      <Contact />
     </div>
     </I18nextProvider>
   );
