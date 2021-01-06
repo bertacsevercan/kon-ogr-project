72d63bee5acf341eb9868186386f3108a742148d
bertacsevercan <bertac.severcan@gmail.com>
2021-01-07 01:18:40 +0300

    add img and content of home jumbotron

--- portfolyo/src/containers/Home/index.jsx
+++ portfolyo/src/containers/Home/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import pcImg from "../../images/pcImg.png";
+import { useTranslation } from "react-i18next";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import "./style.css";
 
 const Home = () => {
-  return <div id="home"></div>;
+  const { t } = useTranslation();
+
+  return (
+    <div id="home">
+      <Jumbotron id="jumbo">
+        <div className="titles">
+          <h1>{t("home.titles.title0")}</h1>
+          <h2>{t("home.titles.title1")}</h2>
+          <div className="summary-wrapper">
+            <p className="summary-text">
+              {t("home.texts.text0")}
+              <br />
+              {t("home.texts.text1")}
+              <br />
+              {t("home.texts.text2")}
+              <br />
+              {t("home.texts.text3")}
+            </p>
+          </div>
+          <Button href="#contact">{t("contact.titles.title0")}</Button>
+        </div>
+        <img width="500px" height="500px" src={pcImg} alt="computer" />
+      </Jumbotron>
+    </div>
+  );
 };
 
 export default Home;
