cb3798cda70c49384e0da95b9f1c6aefde726ee2
bertacsevercan <bertac.severcan@gmail.com>
2021-01-06 23:15:44 +0300

    import bootstrap and i18n

--- portfolyo/src/App.js
+++ portfolyo/src/App.js
@@ -1,10 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 import './App.css';
 
 function App() {
   return (
+    <I18nextProvider i18n={i18n}>
     <div className="App">
    
     </div>
+    </I18nextProvider>
   );
 }
 
